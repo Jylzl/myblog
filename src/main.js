373b8215455777b1f79ef7f0c0b2336302c6fa71
lizilong <94648929@qq.com>
2019-10-17 21:30:14 +0800

    element

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+/**
+ * @description: Description
+ * @author: lizlong<94648929@qq.com>
+ * @since: 2019-10-17 12:51:19
+ * @LastAuthor: lizlong
+ * @lastTime: 2019-10-17 12:51:19
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './plugins/element.js'
+import './plugins/element/element.js'
 
 Vue.config.productionTip = false
 
